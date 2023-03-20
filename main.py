0e84967adf51b545229dec1bf6482183aa76ddf5
yas-say <yasowant@live.com>
2023-03-20 00:32:41 -0400

    missed states added to csv

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ tim.penup()
 state_list = pandas.read_csv("50_states.csv")
 count = 0
 guessed_list = []
+state_not_guessed = []
 while count < len(state_list):
     guess = screen.textinput(f"Guess the state name?{count}/{len(state_list)}", "Enter a state name:")
     print(guess.title())
@@ -31,6 +32,19 @@ while count < len(state_list):
             count += 1
             tim2.goto(int(state_subset.x), int(state_subset.y))
             tim2.write(guess.title(), align="center", font=("Courier", 10, "normal"))
+
+    if guess.lower() == "exit":
+        state_not_guessed = state_list["state"].to_list()
+        print(state_not_guessed)
+        count = 51
+        for _ in guessed_list:
+            state_not_guessed.pop(state_not_guessed.index(_))
+        print(state_not_guessed)
+
+dict = {"state" : state_not_guessed}
+data_dict = pandas.DataFrame(dict)
+data_dict.to_csv("missed_state.csv")
+
 tim2.goto(0, 0)
 tim2.write("GAME OVER", align="center", font=("Courier", 25, "normal"))
 screen.exitonclick()
