b2024085bf4076bad9b7402b0d7e76cc679121dc
yas-say <yasowant@live.com>
2023-03-19 23:59:28 -0400

    finaol

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import pandas
-from turtle import Turtle, Screen
-
+from turtle import Screen, Turtle
 
 screen = Screen()
+tim = Turtle()
+tim2 = Turtle()
+tim2.hideturtle()
+tim2.penup()
 screen.title("US States Game")
 screen.addshape("blank_states_img.gif")
-turtle.shape("blank_states_img.gif")
+tim.shape("blank_states_img.gif")
+tim.penup()
 
-answer = screen.textinput("Guess the state name?", "Enter a state name:")
-print(answer)
+state_list = pandas.read_csv("50_states.csv")
+count = 0
+guessed_list = []
+while count <= len(state_list):
+    guess = screen.textinput(f"Guess the state name?{count}/{len(state_list)}", "Enter a state name:")
+    print(guess.title())
+    # guess = input(f"You have guessed {count}/{len(state_list)}; Enter a state name: ").title()
+    # print(guess)
+    state_subset = state_list[state_list["state"] == guess.title()]
 
-state_list = pandas.read_csv()
+    # print(f"row count {len(state_subset)}")
+    # input()
+    if len(state_subset) > 0:
+        if guess.title() not in guessed_list:
+            guessed_list.append(guess.title())
+            print(state_subset)
+            print(f"X = {int(state_subset.x)} and Y = {int(state_subset.y)}")
+            count += 1
+            tim2.goto(int(state_subset.x), int(state_subset.y))
+            tim2.write(guess.title(), align="center", font=("Courier", 10, "normal"))
 
 screen.exitonclick()
