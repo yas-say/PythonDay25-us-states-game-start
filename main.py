fbcf6aee635d27814234bafba76619318ab5b482
yas-say <yasowant@live.com>
2023-03-20 21:36:09 -0400

    added list compre to missed state

--- main.py
+++ main.py
@@ -34,12 +34,16 @@ while count < len(state_list):
             tim2.write(guess.title(), align="center", font=("Courier", 10, "normal"))
 
     if guess.lower() == "exit":
-        state_not_guessed = state_list["state"].to_list()
-        print(state_not_guessed)
+        # state_not_guessed = state_list["state"].to_list()
+        # print(state_not_guessed)
         count = 51
-        for _ in guessed_list:
-            state_not_guessed.pop(state_not_guessed.index(_))
+        # for _ in guessed_list:
+        #     state_not_guessed.pop(state_not_guessed.index(_))
+        # print(state_not_guessed)
+        state_not_guessed = [num for num in state_list["state"].to_list() if num not in guessed_list]
         print(state_not_guessed)
+        input()
+
 
 dict = {"state" : state_not_guessed}
 data_dict = pandas.DataFrame(dict)
